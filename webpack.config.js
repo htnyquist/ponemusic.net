61f339e0c007862736bb5c0a775c945ad7e84a4f
H.T. Nyquist <htnyquist@gmail.com>
2019-02-23 13:27:29 +0100

    Add base project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: "dist",
+        watchContentBase: true,
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('precss')(),
+                                require('autoprefixer')(),
+                                require('cssnano')()
+                            ]
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
