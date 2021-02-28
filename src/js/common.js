2d5c3c54da523dbb17e1d3bd6e7feba10085b772
H.T. Nyquist <htnyquist@gmail.com>
2021-02-28 18:02:37 +0100

    Update random taglines and Soon page

--- src/js/common.js
+++ src/js/common.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
         "Looking for that one forgotten song?",
         "Looking for the top stuff? Check the albums!",
         "Over 2000 hours of music!",
-        "Enough content to survive G5!",
         "Party like it's 2012 again",
         "Everlasting nostalgia stash",
         "We love FLACs",
         "Way too many Winter Wrap Up remixes",
         "Try a random folder, get hidden gems (or not!)",
         "Now with 20% more covert art!",
-        "800 GB of music? That's an academy record!",
+        "900 GB of pony music? That's an academy record!",
         "Hard drive sold separately",
         "Find something you like? Support your favorite artists!",
         "Ten More Years!",
+        "Now with G5 songs!",
     ];
 
     let lead = document.getElementById('title-lead');
